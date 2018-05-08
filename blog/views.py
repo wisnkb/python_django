cdc885e687d3106ad75ad1c26bf4b861e5dd62ef
wisnkb <wisnkb@gmail.com>
2018-05-08 17:31:38 +0900

    장고 first commmit

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
-# Create your views here.
+# 글목록 조회
 def post_list(request):
-       #name = 'Djangp'
-       # return HttpResponse('''<h1>Hello {myname}</h1>'''.format(myname=name))
-       #return render(request, 'blog/post_list.html')
-       posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
-       return render(request, 'blog/post_list.html',{'posts':posts })
+    # name = '장고~~'
+    # return HttpResponse('''<h1>Hello {myname} </h1>'''.format(myname=name))
+    #queryset 사용해서 Post 목록 가져오기
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/post_list.html',{'posts':posts})
+
+# 글상세조회
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
