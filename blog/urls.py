80a317935f63329729b0f30185667b1634c7f42d
wisnkb <wisnkb@gmail.com>
2018-05-09 14:26:50 +0900

    글수정

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [
-    url(r'^$',views.post_list,name='post_list'),
+        url(r'^$',views.post_list,name='post_list'),
     #localhost:8000/post/1
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/new/$', views.post_new_form, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
-from blog.modelforms import PostModelForm
+from blog.modelforms import PostModelForm, PostForm
 from .models import Post
 
 # 글목록 조회
@@ -18,11 +18,50 @@ def post_detail(request,pk):
     post = get_object_or_404(Post,pk=pk)
     return render(request,'blog/post_detail.html',{'post':post})
 
-# 글등록
+#글등록 ModelForm 사용
 def post_new(request):
-    form = PostModelForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    if request.method == "POST":
+        form = PostModelForm(request.POST)
+        print(form)
+        if form.is_valid():
+            post = form.save(commit=False)
+        post.author = request.user
+        post.published_date = timezone.now()
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostModelForm()
+        print(form)
+    return render(request,'blog/post_edit.html',{'form':form})
 
+#글등록 Form을 사용
+def post_new_form(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        #print(form)
+        if form.is_valid():
+            print(form.cleaned_data)
+            post = Post(author=request.user,
+                        title=form.cleaned_data['title'],text=form.cleaned_data['text'],published_date=timezone.now()
+                        )
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+        else:   #검증실패
+            print(form.errors)
+    else:
+        form = PostForm()
+    return render(request,'blog/post_form.html',{'form':form})
 
-
-
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostModelForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostModelForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
